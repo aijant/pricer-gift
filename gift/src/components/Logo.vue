f4b17428cfe7fa8e4d191f3b693e91098de86cee
Aizhan Tilek <aijanm01@gmail.com>
2021-07-14 09:54:02 +0600

    Added Logo page

---
+++ gift/src/components/Logo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="contents__logo">
+    <a href="https://crosset.onward.co.jp/mag/" target="_blank">
+      <h1 class="mod-logoMain">
+        <img src="../assets/ttl-mag_brown.png" alt="ONWARD MAG" />
+      </h1>
+    </a>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.contents__logo {
+  width: 179px;
+  height: auto;
+  margin: 0 auto 40px;
+
+  a {
+    color: $main-color;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .mod-logoMain {
+    width: 100%;
+    height: auto;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    vertical-align: bottom;
+  }
+}
+</style>
--- gift/src/components/MainContent.vue
+++ gift/src/components/MainContent.vue
@@ -1,13 +1,7 @@
 <template>
   <main>
     <section class="contents">
-      <div class="contents__logo">
-        <a href="https://crosset.onward.co.jp/mag/" target="_blank">
-          <h1 class="mod-logoMain">
-            <img src="../assets/ttl-mag_brown.png" alt="ONWARD MAG" />
-          </h1>
-        </a>
-      </div>
+      <logo />
       <p class="contents__lead-img">
         <picture>
           <source
@@ -35,8 +29,13 @@
 </template>
 
 <script>
+import Logo from './Logo'
+
 export default {
   name: 'MainContent',
+   components: {
+   logo: Logo
+   },
 }
 </script>
 
@@ -46,29 +45,6 @@ export default {
   margin: 0 auto;
   padding-bottom: 94px;
   font-family: $font_sans;
-}
-
-.contents__logo {
-  width: 179px;
-  height: auto;
-  margin: 0 auto 40px;
-
-  a {
-    color: $main-color;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
-  .mod-logoMain {
-    width: 100%;
-    height: auto;
-  }
-
-  img {
-    width: 100%;
-    height: auto;
-    vertical-align: bottom;
-  }
 }
 
 .contents__lead-img {
